bf8dd566a469de8c52d1f327378f88fa7d3f0219
Rus David <davidrus.art@gmail.com>
2017-06-22 12:53:30 +0300

    Initial commit

---
+++ src/main/java/com/davidrus/smarthouse/SmarthouseApplication.java
@@ -0,0 +1,12 @@
+package com.davidrus.smarthouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmarthouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmarthouseApplication.class, args);
+	}
+}
